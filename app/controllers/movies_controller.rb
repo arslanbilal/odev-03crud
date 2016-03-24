9fcd63216d2c7c3d66d014209821a20c6eab5fd0
Bilal Arslan <bilal.arslan1993@hotmail.com>
2016-03-24 03:53:41 +0200

    Add Create and Delete operations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,11 +12,22 @@ class MoviesController < ApplicationController
 
   def new
   	# new_movie GET    /movies/new(.:format)
-  	@movie = Movie.new
   end
 
   def create
   	# - 	POST   /movies(.:format)
+    movie = Movie.new
+    movie.title = params[:title]
+    movie.director = params[:director]
+    movie.description = params[:description]
+    movie.country = params[:country]
+    movie.date = params[:date]
+
+    if movie.save
+      redirect_to movies_path
+    else
+      redirect_to new_movie_path, notice: "Can not added."
+    end
   end
 
   def edit
@@ -30,6 +41,15 @@ class MoviesController < ApplicationController
     
   def destroy
   	# DELETE /movies/:id(.:format)
+    movie = Movie.find(params[:id])
+    message = ""
+    if movie.destroy
+      message = " succesfully deleted."
+    else
+      message = " cannot deleted."
+    end
+
+    redirect_to movies_path, notice: movie.title + message
   end
 
 end
